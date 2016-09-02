b892d2e7a4908863036df59fe0e243185e8f9541
materok <marcel.materok@rwth-aachen.de>
2016-09-02 09:07:39 +0200

    updates

--- python/stats.py
+++ python/stats.py
@@ -7,14 +7,15 @@ from helper.laufen import *
 #from datetime import date
 def main():
 
-    #km5,t5,bpm,day,km1,t1,km2,t2,km3,t3,km4,t4= np.genfromtxt('data.txt', unpack=True)
-    gew,day= np.genfromtxt('../stats.txt', unpack=True)
+    t5,km5,bpm,bpm_max,day= np.genfromtxt('../dataLight.txt', unpack=True)
+    day=new_arr(day)
+    gew,day1= np.genfromtxt('../stats.txt', unpack=True)
     #km5 and t5 are complete distance traveled and time needed
     gew=new_arr(gew)
-    day=new_arr(day)
+    day1=new_arr(day1)
 
-    c1 = TCanvas("gewicht","gewicht",200,9,700,500)
-    MakeStats(c1,day,gew)
+    c1 = TCanvas("gewicht","gewicht",200,9,1400,500)
+    MakeStats(c1,day1,gew,day)
 
 
 if __name__=="__main__":
