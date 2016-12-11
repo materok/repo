69738d003b202d9e92e67aaf2ae468eb490b59cc
materok <noxxas@web.de>
2016-12-11 12:45:34 +0100

    updated mplt plotter, included beamer setup, will not compile correctly due to missing files in src

--- python/mpltlib/stats.py
+++ python/mpltlib/stats.py
@@ -1,21 +1,12 @@
 import numpy as np
-from ROOT import *
-#from helper.helper import tConvert,calcVelo,makeCumul,percentage,new_arr,diff,delta
+import matplotlib.pyplot as plt
 from helper.laufen import *
-#import os
-#import time
-#from datetime import date
+
 def main():
 
     t5,km5,bpm,bpm_max,day= np.genfromtxt('../../dataLight.txt', unpack=True)
-    day=day.astype('d')
     gew,day1= np.genfromtxt('../../stats.txt', unpack=True)
-    #km5 and t5 are complete distance traveled and time needed
-    gew=gew.astype('d')
-    day1=day1.astype('d')
-
-    c1 = TCanvas("gewicht","gewicht",200,9,1400,500)
-    MakeStats(c1,day1,gew,day)
+    makeStats(day1,gew,2016,day)
 
 
 if __name__=="__main__":
