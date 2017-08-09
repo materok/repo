c07d765d67d3fd1b2a403d5cd4b9f6acf6799e6a
mrcl <materok@physik.rwth-aachen.de>
2017-08-09 09:38:43 +0200

    update

--- python/mpltlib/laufen.py
+++ python/mpltlib/laufen.py
@@ -51,7 +51,70 @@ def main(version=17):
         MakeDeltaPlot(day17,month17,gew17,year,savepng=True)
         #~ MakeDeltaPlots(day17,month17,gew17,year,savepng=True)
         MakeComposition(day17p2,month17p2,fett,wasser,muskel,knochen,year,savepng=True)
+    elif version==0:
+        year=17
+        gew17,day17,month17= np.genfromtxt('../../stats17.txt',missing_values=",", filling_values = -1, unpack=True)
+        gew17p2,day17p2,month17p2,fett,wasser,muskel,knochen= np.genfromtxt('../../stats17p2.txt',missing_values=",", filling_values = -1, unpack=True)
+        gew17=np.append(gew17,gew17p2)
+        gew17/=10.
+        fett/=10.
+        wasser/=10.
+        muskel/=10.
+        knochen/=10.
+        day17=np.append(day17,day17p2)
+        month17=np.append(month17,month17p2)
+        fillEmpty(month17)
+        fillEmpty(month17p2)
+        plt.figure(figsize=(20,10))
+        y=gew17
+        day,month=day17,month17
+        x=dayAndMonthToBin(day,month,year)
+        yRun=np.zeros(len(y))
+        yerrRun=np.zeros(len(y))
+        yerr=np.zeros(len(y))
+        y_bmi=np.zeros(len(y))
+        yerr_bmi=np.zeros(len(y))
+        possibleLabels = ['January', 'Febuary', 'March', 'April',
+                          'May','June','July','August','September',
+                          'Oktober','November', 'December']
+        labels=[]
+        x_ticks=[]
+        for i in range(len(month)):
+            if (month[i]==month[i-1]) and i != 0:
+                labels.append('')
+            else:
+                labels.append(possibleLabels[int(month[i])-1])
+                x_ticks.append(day[i])
+        counter=0
+        for i in range(len(y)):
+            if i < 5:
+                yerr[i]=var(y[:5])**.5
+            else:
+                yerr[i]=var(y[i-5:i])**.5
+                if yerr[i]<0.1:
+                    yerr[i]=0.1
+        import subprocess
+        from scipy.optimize import curve_fit
+        def fit_func(x,a,b):
+            return a*x+b
+        fitStart=np.where(y == y.max())
+        print fitStart
+        popt, pcov = curve_fit(func, x, y)
+        print popt
+        #~ plt.plot(xdata, func(xdata, *popt), 'r-', label='fit')
+        plt.figure(figsize=(10,10))
+        plt.errorbar(x, y, xerr=0.25, yerr=yerr, fmt='o',zorder=1)
+        minIndices=np.where(y == y.min())
+        plt.plot(x[minIndices], y[minIndices], 'h',zorder=6,color="green")
+        plt.xlabel("month")
+        plt.xticks(x, labels, rotation='vertical')
+        plt.subplots_adjust(bottom=0.175)
+        plt.ylabel("weight in kg")#
+        plt.show()
+
+
 
 if __name__=="__main__":
     main()
+    #~ main(0)
     #main(16)
